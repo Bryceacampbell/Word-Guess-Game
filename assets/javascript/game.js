3b8b3ead965b44a40e9b103f3efcdf58819d7026
germaine <bryceacampbell7@gmail.com>
2019-03-04 17:05:53 -0700

    few changes to startGame function and logic function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,53 @@
 var userGuess;
-//create object called game
+
 var game = {
 
-    //create an array with words to guess
     wordBank: ["vanilla", "chocolate", "cookie dough", "mint chocolate chip", "rocky road"],
-
-    //declare a variable for the amount of guesses a user has
     guessesLeft: 12,
-
-    //set wins to 0
     wins: 0,
-
-    //set losses to 0
     losses: 0,
-
-    //create an empty string to store the random word the computer will generate out of wordBank[]
     randomWord: "",
+    underscores: "",
 
     //create a function to determine what happens when the game is started
-    startGame: function() {
+    startGame: function () {
 
         //math function to pick a random word out of wordBank[] and stores it into a variable called randomWord
         this.randomWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+        console.log(this.randomWord);
 
         //for loop to create underscores for each letter of the chosen randomWord
+        var underscores;
         for (var i = 0; i < this.randomWord.length; i++) {
-            randomWord[i] = "_";
-        }        
-    }
-
-    gameLogic: function() {
-
-        for (var j = 0; j < this.randomWord.length; j++) {
-            
+            underscores += "_ ";
         }
-        
+        return underscores
+    },
+
+    //checker function to check if user guess is equal to the indexOf randomWord
+    checker: function () {
+        var guessIndex = this.randomWord.indexOf(userGuess);
+        if (!guessIndex) {
+            this.guessesLeft--;
+            console.log(false);
+            // dont update `underscores`
+        } 
+        else {
+            this.underscores[guessIndex] = userGuess;
+            this.guessesLeft--;
+            console.log(true)
+        }
+
     }
-    
 }
 
+//onkeyup event listener to determine what key is being pressed
+document.onkeyup = function (event) {
 
+    userGuess = event.key;
 
+    //initiate checker function
+    game.checker();
 
-
-
-
-
-
-
-
-
-
-// //create array with words to guess
-// var flavorWords = ["vanilla", "chocolate", "cookie dough", "mint chocolate chip", "rocky road"];
-
-// //go through flavorsWords and pick a random word to start
-// var randomWord = flavorWords[Math.floor(Math.random() * flavorWords.length)];
-// console.log(randomWord);
-
-// hiddenWord = document.getElementById("hidden-word");
-// winsCounter = document.getElementById("wins-counter");
-
-// var wins = 0;
-
-// //create an empty array and a for loop to match the random word chosen with underscores _
-// var answerArray = [];
-// for (var i = 0; i < randomWord.length; i++) {
-//     answerArray[i] = "_";
-//     hiddenWord.textContent = answerArray;
-// }
-
-
-
+    console.log(userGuess);
+}
