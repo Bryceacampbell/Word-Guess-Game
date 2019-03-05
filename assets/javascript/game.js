a9ad71aec158c7599695fcb6351b6240297e6515
John and Kari <bryceacampbell7@gmail.com>
2019-03-04 20:27:29 -0700

    gettin somewhere

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,12 @@
+//gloabal variables
 var userGuess;
+
+String.prototype.replaceAt = function () {
+    for (var j = 0; j < this.randomWord.length; j++) {
+        this.underscores = game.replaceAt(this.underscores, j, userGuess);
+        document.getElementById("hidden-word").innerHTML = this.underscores;
+    }
+}
 
 var game = {
 
@@ -9,6 +17,8 @@ var game = {
     randomWord: "",
     underscores: "",
 
+    replaceAt: function (string, index, replace) { return string.substring(0, index) + replace + string.substring(index + 1); },
+
     //create a function to determine what happens when the game is started
     startGame: function () {
 
@@ -17,11 +27,7 @@ var game = {
         console.log(this.randomWord);
 
         //for loop to create underscores for each letter of the chosen randomWord
-        var underscores;
-        for (var i = 0; i < this.randomWord.length; i++) {
-            underscores += "_ ";
-        }
-        return underscores
+    
     },
 
     //checker function to check if user guess is equal to the indexOf randomWord
@@ -29,13 +35,13 @@ var game = {
         var guessIndex = this.randomWord.indexOf(userGuess);
         if (!guessIndex) {
             this.guessesLeft--;
-            console.log(false);
+            
             // dont update `underscores`
-        } 
+        }
         else {
             this.underscores[guessIndex] = userGuess;
             this.guessesLeft--;
-            console.log(true)
+            console.log(guessIndex);
         }
 
     }
@@ -51,3 +57,4 @@ document.onkeyup = function (event) {
 
     console.log(userGuess);
 }
+
